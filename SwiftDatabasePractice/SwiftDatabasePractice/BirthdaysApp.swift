c8c5069bd8415d7468f51a628091b11b037ea6c9
Mika Grönroos <mika.gronroos2@metropolia.fi>
2025-02-28 13:46:23 +0200

    SwiftDatabaseYupdate1

---
+++ SwiftDatabasePractice/SwiftDatabasePractice/BirthdaysApp.swift
@@ -0,0 +1,18 @@
+//
+//  BirthdaysApp.swift
+//  SwiftDatabasePractice
+//
+//  Created by Mika Grönroos on 28.2.2025.
+//
+
+import SwiftUI
+
+struct BirthdaysApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BirthdaysApp()
+}
---
+++ SwiftDatabasePractice/SwiftDatabasePractice/Friend.swift
@@ -0,0 +1,8 @@
+//
+//  Friend.swift
+//  SwiftDatabasePractice
+//
+//  Created by Mika Grönroos on 28.2.2025.
+//
+
+import Foundation
